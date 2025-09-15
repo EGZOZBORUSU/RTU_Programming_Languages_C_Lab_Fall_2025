544855b1e38c37184afe851824dface9bb0dded4
Zeron <84472661+EGZOZBORUSU@users.noreply.github.com>
2025-09-15 07:48:30 +0000

    Update hello message

--- src/hello.c
+++ src/hello.c
@@ -5,8 +5,12 @@ int main(int argc, char *argv[]) {
   // This is your first C program my friend
   printf("Hello, RTU World from C Lab in 2025!\n");
   printf("You passed %d argument(s).\n", argc - 1);
+
+  printf("Hello from Eren Efe Ozkan! \n");
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
+
+    ;
   }
   return 0;
 }
